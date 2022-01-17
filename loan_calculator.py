ccd74bb216c7404b9f96acad7b64e665a7ab2fa5
Owen JP <owenjp311@gmail.com>
2022-01-17 11:14:55 +0000

    initial commit

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,3 +1,4 @@
+
 def calc_money_owed():  # function to get the loan details
     while True:
         value = (input("How much is owed in GBP?: £"))
@@ -10,7 +11,6 @@ def calc_apr_rate():
         value = (input("What is the annual percentage rate?: "))
         if float_validation(value):  # if input is valid (true) move on
             return value
-        return value
 
 
 def calc_payment():
@@ -18,15 +18,20 @@ def calc_payment():
         value = (input("What is your monthly payment in GBP?: £"))
         if float_validation(value):  # if input is valid (true) move on
             return value
-        return value
 
 
-def calc_months():
+def calc_months_remaining():
     while True:  # loop until valid input
-        value = (input("How many months until full payback?: "))
+        value = (input("How many months left on the term?: "))
         if int_validation(value):  # if input is valid (true) move on
             return value
-        return value
+
+
+def calc_loan_term():
+    while True:  # loop until valid input
+        value = (input("How many months is the loan term?: "))
+        if int_validation(value):  # if input is valid (true) move on
+            return value
 
 
 def float_validation(value):  # function to validate input that should be a float
@@ -46,26 +51,29 @@ def int_validation(value):  # function to validate input that should be an int
 
 
 def print_results(payment, interest_paid, money_owed):
-    print("£", payment, "has been payed, of which £", interest_paid, "was interest\n")
-    print("£", money_owed, "is still owed")
-
+    print("£", f'{payment:.2f}', "has been payed, of which £", f'{interest_paid:.2f}', "was interest\n")
+    print("£", f'{money_owed:.2f}', "is still owed")
+    # f'{variable:.2f}' this formats the enclosed variable to 2 decimal places (using f strings)
 
 def main():
     # instantiate variables
     money_owed = 0
     apr_rate = 0
     payment = 0
-    months = 0
+    loan_term = 0
+    months_remaining = 0
+
 
     # assign them values as per user input
     money_owed = float(calc_money_owed())
     apr_rate = float(calc_apr_rate())
     payment = float(calc_payment())
-    months = int(calc_months())
-
+    loan_term = int(calc_loan_term())
+    months_remaining = int(calc_months_remaining())
 
     # calculate loan details
-    monthly_rate = apr_rate / 100 / 12
+    payment = payment * (loan_term - months_remaining)
+    monthly_rate = apr_rate / 100 / (loan_term - months_remaining)
     interest_paid = money_owed * monthly_rate
     money_owed = (money_owed + interest_paid) - payment
 
